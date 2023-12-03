62d0e3e8aa488a73c45aaefb14ea0c1af95b2c39
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-03 09:06:18 -0300

    chore: another way to do the same solution

--- 2023/rust/day02/src/lib.rs
+++ 2023/rust/day02/src/lib.rs
@@ -49,102 +49,148 @@ pub fn process_part1(input: &str) -> u32 {
 }
 
 pub fn process_part2(input: &str) -> u32 {
-    input
-        .trim()
-        .split("\n")
-        .map(|n| n.to_string())
-        .collect::<Vec<String>>()
-        .iter()
-        .map(|line| {
-            let game = get_sets_from_line(line);
+    let lines = input
+        .lines()
+        .map(|x| x.split(':').skip(1).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    println!(" ");
+    println!("input {:?}", lines);
 
-            let colors = game
-                .groups
-                .into_iter()
-                .flatten()
-                .collect::<Vec<ColorCount>>();
+    let mut arry_multiples = vec![];
+    for (_index, line) in lines.iter().enumerate() {
+        let sep = line.join("");
+        let sep = sep.split(';').collect::<Vec<_>>();
 
-            let red = colors
-                .iter()
-                .filter(|col| col.color == Colors::Red)
-                .max_by_key(|col| col.number)
-                .unwrap()
-                .number as usize;
-            let green = colors
-                .iter()
-                .filter(|col| col.color == Colors::Green)
-                .max_by_key(|col| col.number)
-                .unwrap()
-                .number as usize;
-            let blue = colors
-                .iter()
-                .filter(|col| col.color == Colors::Blue)
-                .max_by_key(|col| col.number)
-                .unwrap()
-                .number as usize;
+        let (mut red, mut green, mut blue) = (("red", 0), ("green", 0), ("blue", 0));
 
-            (red * green * blue) as u32
-        })
-        .sum()
-}
+        for (it_index, it) in sep.iter().enumerate() {
+            let ab = it.split(',').map(|x| x.trim()).collect::<Vec<_>>();
 
-#[derive(Debug, PartialEq)]
-enum Colors {
-    Red,
-    Blue,
-    Green,
-}
+            for bc in ab {
+                let split = bc.split(' ').collect::<Vec<_>>();
+                let num = split
+                    .first()
+                    .expect("must be num")
+                    .parse::<u32>()
+                    .expect("is number");
+                let color = split.last().expect("must be num");
 
-#[derive(Debug)]
-struct ColorCount {
-    color: Colors,
-    number: u8,
-}
-
-#[derive(Debug)]
-struct Game {
-    id: u8,
-    groups: Vec<Vec<ColorCount>>,
-}
-
-fn get_sets_from_line(line: &str) -> Game {
-    let split: Vec<&str> = line.split(":").collect();
-    let label_str = split.first().unwrap();
-    let game_id = label_str.replace("Game ", "").parse::<u8>().unwrap();
-
-    let groups_str = split.last().unwrap().trim();
-    let groups_vec = groups_str.split(";").collect::<Vec<&str>>();
-    let groups: Vec<Vec<ColorCount>> = groups_vec
-        .iter()
-        .map(|group| {
-            group
-                .split(",")
-                .collect::<Vec<&str>>()
-                .iter()
-                .map(|color| {
-                    let split: Vec<&str> = color.trim().split_whitespace().collect();
-                    let number = split.first().unwrap().parse::<u8>().unwrap();
-                    let col = split.last().unwrap().to_owned();
-
-                    ColorCount {
-                        color: match col {
-                            "red" => Colors::Red,
-                            "blue" => Colors::Blue,
-                            "green" => Colors::Green,
-                            _ => panic!(),
-                        },
-                        number,
-                    }
-                })
-                .collect()
-        })
-        .collect();
-
-    Game {
-        id: game_id,
-        groups,
+                if color == &red.0 {
+                    red.1 = if red.1 > num { red.1 } else { num }
+                }
+                if color == &green.0 {
+                    green.1 = if green.1 > num { green.1 } else { num }
+                }
+                if color == &blue.0 {
+                    blue.1 = if blue.1 > num { blue.1 } else { num }
+                }
+            }
+            let num = red.1 * green.1 * blue.1;
+            if it_index == sep.len() - 1 {
+                arry_multiples.push(num);
+            }
+        }
     }
+    arry_multiples.iter().sum()
 }
+
+// pub fn process_part2(input: &str) -> u32 {
+//     input
+//         .trim()
+//         .split("\n")
+//         .map(|n| n.to_string())
+//         .collect::<Vec<String>>()
+//         .iter()
+//         .map(|line| {
+//             let game = get_sets_from_line(line);
+//
+//             let colors = game
+//                 .groups
+//                 .into_iter()
+//                 .flatten()
+//                 .collect::<Vec<ColorCount>>();
+//
+//             let red = colors
+//                 .iter()
+//                 .filter(|col| col.color == Colors::Red)
+//                 .max_by_key(|col| col.number)
+//                 .unwrap()
+//                 .number as usize;
+//             let green = colors
+//                 .iter()
+//                 .filter(|col| col.color == Colors::Green)
+//                 .max_by_key(|col| col.number)
+//                 .unwrap()
+//                 .number as usize;
+//             let blue = colors
+//                 .iter()
+//                 .filter(|col| col.color == Colors::Blue)
+//                 .max_by_key(|col| col.number)
+//                 .unwrap()
+//                 .number as usize;
+//
+//             (red * green * blue) as u32
+//         })
+//         .sum()
+// }
+//
+// #[derive(Debug, PartialEq)]
+// enum Colors {
+//     Red,
+//     Blue,
+//     Green,
+// }
+//
+// #[derive(Debug)]
+// struct ColorCount {
+//     color: Colors,
+//     number: u8,
+// }
+//
+// #[derive(Debug)]
+// struct Game {
+//     id: u8,
+//     groups: Vec<Vec<ColorCount>>,
+// }
+//
+// fn get_sets_from_line(line: &str) -> Game {
+//     let split: Vec<&str> = line.split(":").collect();
+//     let label_str = split.first().unwrap();
+//     let game_id = label_str.replace("Game ", "").parse::<u8>().unwrap();
+//
+//     let groups_str = split.last().unwrap().trim();
+//     let groups_vec = groups_str.split(";").collect::<Vec<&str>>();
+//     let groups: Vec<Vec<ColorCount>> = groups_vec
+//         .iter()
+//         .map(|group| {
+//             group
+//                 .split(",")
+//                 .collect::<Vec<&str>>()
+//                 .iter()
+//                 .map(|color| {
+//                     let split: Vec<&str> = color.trim().split_whitespace().collect();
+//                     let number = split.first().unwrap().parse::<u8>().unwrap();
+//                     let col = split.last().unwrap().to_owned();
+//
+//                     ColorCount {
+//                         color: match col {
+//                             "red" => Colors::Red,
+//                             "blue" => Colors::Blue,
+//                             "green" => Colors::Green,
+//                             _ => panic!(),
+//                         },
+//                         number,
+//                     }
+//                 })
+//                 .collect()
+//         })
+//         .collect();
+//
+//     Game {
+//         id: game_id,
+//         groups,
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
