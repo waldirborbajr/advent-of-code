00ab945b0a553a6f608db2a9f85f3e612692b19b
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-04 10:07:52 -0300

    feat: add day 4

--- 2023/rust/day04/src/bin/part-1.rs
+++ 2023/rust/day04/src/bin/part-1.rs
@@ -3,5 +3,15 @@ use day04::process_part1;
 fn main() {
     let file = include_str!("../../input01.txt");
 
-    println!("{}", process_part1(file));
+    let starttime = std::time::Instant::now();
+
+    let output = process_part1(file);
+    dbg!(output);
+
+    let elapsed = starttime.elapsed();
+    println!(
+        "took {}ms ({}us)  ",
+        elapsed.as_millis(),
+        elapsed.as_micros()
+    );
 }
--- 2023/rust/day04/src/bin/part-2.rs
+++ 2023/rust/day04/src/bin/part-2.rs
@@ -3,5 +3,15 @@ use day04::process_part2;
 fn main() {
     let file = include_str!("../../input01.txt");
 
-    println!("{}", process_part2(file));
+    let starttime = std::time::Instant::now();
+
+    let output = process_part2(file);
+    dbg!(output);
+
+    let elapsed = starttime.elapsed();
+    println!(
+        "took {}ms ({}us)  ",
+        elapsed.as_millis(),
+        elapsed.as_micros()
+    );
 }
