b3d0734e388f90311c1af3d0b8810104a643899c
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-08 08:30:58 -0300

    feat: comming soon

---
+++ 2023/rust/day09/src/bin/part-1.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+use regex::Regex;
+
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let starttime = std::time::Instant::now();
+
+    let answer = process(input);
+    println!("Part 1 answer: {answer}");
+
+    let elapsed = starttime.elapsed();
+    println!(
+        "took {}ms ({}us)  ",
+        elapsed.as_millis(),
+        elapsed.as_micros()
+    );
+}
+
+pub fn process(input: &str) -> i64 {
+    todo!("process input")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_process() {
+        let input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+        assert_eq!(2, process(input))
+    }
+}
