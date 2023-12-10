d5d190913c60baa64a7f3dc4cdcd2476da67cd67
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-10 10:12:51 -0300

    feat: comming soon

---
+++ 2023/rust/day11/src/bin/part-2.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let input = include_str!("../../input.txt");
+
+    let starttime = std::time::Instant::now();
+
+    let answer = process(input);
+    println!("Part 2 answer: {answer}");
+
+    let elapsed = starttime.elapsed();
+    println!(
+        "took {}ms ({}us)  ",
+        elapsed.as_millis(),
+        elapsed.as_micros()
+    );
+}
+
+pub fn process(input: &str) -> i32 {
+    todo!("Day 11 part 2")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_process() {
+        let input = "";
+        assert_eq!(1, process(input))
+    }
+}
