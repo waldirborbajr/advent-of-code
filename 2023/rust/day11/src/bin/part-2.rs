cd70ab7642af5bd584b95c7f0338d5f05dfee14f
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-11 08:55:58 -0300

    feat: day 11 - done

--- 2023/rust/day11/src/bin/part-2.rs
+++ 2023/rust/day11/src/bin/part-2.rs
@@ -14,8 +14,87 @@ fn main() {
     );
 }
 
-pub fn process(input: &str) -> i32 {
-    todo!("Day 11 part 2")
+const SIZE: u64 = 1_000_000;
+
+pub fn process(input: &str) -> u64 {
+    let empty_rows: Vec<u64> = input
+        .lines()
+        .enumerate()
+        .filter(|(_, line)| line.chars().filter(|c| c != &'.').count() == 0)
+        .map(|(i, _)| i as u64)
+        .collect();
+    let empty_cols: Vec<u64> = {
+        let mut iter = input.lines();
+        let mut cols: Vec<char> = iter.next().unwrap().chars().collect();
+        while let Some(current) = iter.next() {
+            for (x, c) in current.chars().enumerate() {
+                if c != '.' {
+                    cols[x] = '#';
+                }
+            }
+        }
+
+        cols.iter()
+            .enumerate()
+            .filter(|(_, c)| **c == '.')
+            .map(|(i, _)| i as u64)
+            .collect()
+    };
+
+    let map: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    let points: Vec<(u64, u64)> = map
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, c)| **c == '#')
+                .map(|(i, _)| (i as u64, y as u64))
+                .collect::<Vec<(u64, u64)>>()
+        })
+        .filter(|line| !line.is_empty())
+        .fold(Vec::new(), |mut v, mut l| {
+            v.append(&mut l);
+            v
+        });
+
+    let len = points.len();
+    let mut sum = 0;
+    for i in 0..len {
+        for j in (i + 1)..len {
+            sum += {
+                let p1 = points[i];
+                let p2 = points[j];
+                let mut dist = 0;
+                for x in p1.0.min(p2.0)..p1.0.max(p2.0) {
+                    if empty_cols.contains(&x) {
+                        dist += SIZE;
+                    } else {
+                        dist += 1;
+                    }
+                }
+                for y in p1.1.min(p2.1)..p1.1.max(p2.1) {
+                    if empty_rows.contains(&y) {
+                        dist += SIZE;
+                    } else {
+                        dist += 1;
+                    }
+                }
+
+                dist
+            }
+        }
+    }
+    //for row in map {
+    //    for c in row {
+    //        print!("{}", c);
+    //    }
+    //    print!("\n");
+    //}
+    //println!("{:?}", points);
+
+    sum as u64
 }
 
 #[cfg(test)]
@@ -24,7 +103,16 @@ mod tests {
 
     #[test]
     fn part1_process() {
-        let input = "";
-        assert_eq!(1, process(input))
+        let input = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+        assert_eq!(8410, process(input))
     }
 }
