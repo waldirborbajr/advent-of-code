f0b3051255b3eb03c57e467c87f51d4a6636f4d1
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-12 07:43:37 -0300

    feat: day 12 - done

--- 2023/rust/day12/src/bin/part-1.rs
+++ 2023/rust/day12/src/bin/part-1.rs
@@ -14,8 +14,97 @@ fn main() {
     );
 }
 
-pub fn process(input: &str) -> u64 {
-    todo!("day 12 - commiing soon")
+pub fn process(input: &str) -> u32 {
+    let lines = input.lines();
+
+    let mut sum_valid: u32 = 0;
+    for line in lines {
+        let splitted_line = line.split(" ").collect::<Vec<&str>>();
+        let springs: Vec<char> = splitted_line[0].chars().collect::<Vec<char>>();
+
+        let ss = splitted_line[1].split(",").collect::<Vec<&str>>();
+        let mut spring_sizes = Vec::new();
+        for s in ss {
+            spring_sizes.push(s.parse::<u32>().unwrap());
+        }
+
+        // let spring_sizes: Vec<u32> = splitted_line[1]
+        //     .split(",")
+        //     .map(|s| s.parse::<u32>().unwrap());
+
+        // Find all valid arrangements
+        let valid_arrangements_springs: u32 = valid_arrangements(springs, spring_sizes);
+        sum_valid += valid_arrangements_springs;
+    }
+
+    sum_valid
+}
+
+fn is_valid(springs: Vec<char>, spring_sizes: Vec<u32>) -> bool {
+    let mut curr_broken = 0;
+    let mut curr_size_i = 0;
+
+    for c in springs.clone() {
+        // Count consecutive broken springs
+        if c == '#' {
+            curr_broken += 1;
+        }
+        // Check if found broken springs match the next spring size
+        else if curr_broken > 0 {
+            if curr_size_i >= spring_sizes.len() {
+                return false;
+            }
+            if curr_broken != spring_sizes[curr_size_i] {
+                return false;
+            }
+            curr_size_i += 1;
+            curr_broken = 0;
+        }
+    }
+
+    // Check last sequence of broken springs
+    if curr_broken > 0 {
+        if curr_size_i >= spring_sizes.len() {
+            return false;
+        }
+        if curr_broken != spring_sizes[curr_size_i] {
+            return false;
+        }
+        curr_size_i += 1;
+    }
+
+    // Check if all spring sizes are used
+    if spring_sizes.len() != curr_size_i {
+        return false;
+    }
+    return true;
+}
+
+fn valid_arrangements(springs: Vec<char>, spring_sizes: Vec<u32>) -> u32 {
+    // Check if all unknowns are filled, check validity
+    if springs.iter().all(|&c| c != '?') {
+        if is_valid(springs, spring_sizes) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    // Find first unknown, start recursion
+    let mut valids = 0;
+    for i in 0..springs.len() {
+        if springs[i] == '?' {
+            let mut springs_copy = springs.clone();
+            springs_copy[i] = '#';
+            valids += valid_arrangements(springs_copy, spring_sizes.clone());
+            let mut springs_copy_2 = springs.clone();
+            springs_copy_2[i] = '.';
+            valids += valid_arrangements(springs_copy_2, spring_sizes.clone());
+            break;
+        }
+    }
+
+    valids
 }
 
 #[cfg(test)]
@@ -24,7 +113,17 @@ mod tests {
 
     #[test]
     fn part1_process() {
-        let input = "";
-        assert_eq!(374, process(input))
+        let input = "?###???????? 3,2,1
+.###.##.#...
+.###.##..#..
+.###.##...#.
+.###.##....#
+.###..##.#..
+.###..##..#.
+.###..##...#
+.###...##.#.
+.###...##..#
+.###....##.#";
+        assert_eq!(21, process(input))
     }
 }
--- 2023/rust/day12/src/bin/part-2.rs
+++ 2023/rust/day12/src/bin/part-2.rs
@@ -14,8 +14,64 @@ fn main() {
     );
 }
 
-pub fn process(input: &str) -> u64 {
-    todo!("day 12 - comming soon")
+pub fn process(input: &str) -> i64 {
+    input
+        .lines()
+        .filter_map(|s| {
+            let (s, rest) = s.split_once(' ')?;
+
+            let b = s.as_bytes();
+            let mut s = Vec::with_capacity((s.len() + 1) * 5);
+            for r in 0..5 {
+                s.extend_from_slice(b);
+                s.push(if r != 4 { b'?' } else { b'.' });
+            }
+
+            let mut xs: Vec<i32> = rest.split(',').filter_map(|x| x.parse().ok()).collect();
+
+            let n = xs.len();
+            for _ in 0..4 {
+                xs.extend_from_within(..n);
+            }
+
+            Some(solve(&s, &xs))
+        })
+        .sum()
+}
+
+fn solve(s: &[u8], xs: &[i32]) -> i64 {
+    let n = s.len();
+    let m = xs.len();
+
+    // (parsed position, number of groups of #, last group len)
+    let x = n + 1;
+    let y = m + 2;
+    let z = n + 2;
+    let mut dp = vec![0i64; x * y * z]; // [i,j,k] = (i * y * z) + (j * z) + k;
+    dp[0] = 1;
+
+    for i in 0..n {
+        for j in 0..=m {
+            for k in 0..=n {
+                let curr = dp[i * y * z + j * z + k]; // [i, j, k]
+                if curr == 0 {
+                    continue;
+                }
+                // current group is zero or of required length -> continue without a group
+                if (s[i] == b'.' || s[i] == b'?') && (k == 0 || k == (xs[j - 1] as usize)) {
+                    dp[(i + 1) * y * z + j * z] += curr; // [i + 1, j, 0]
+                }
+                // continue the previous group or start a new one
+                if s[i] == b'#' || s[i] == b'?' {
+                    let nz = if k == 0 { 1 } else { 0 };
+                    dp[(i + 1) * y * z + (j + nz) * z + (k + 1)] += curr; // [i + 1, j + nz, k + 1]
+                }
+            }
+        }
+    }
+
+    // [n, m, 0] when we parsed all n inputs and matched all m group-lengths
+    dp[n * y * z + m * z]
 }
 
 #[cfg(test)]
@@ -24,7 +80,17 @@ mod tests {
 
     #[test]
     fn part1_process() {
-        let input = "";
-        assert_eq!(8410, process(input))
+        let input = "?###???????? 3,2,1
+.###.##.#...
+.###.##..#..
+.###.##...#.
+.###.##....#
+.###..##.#..
+.###..##..#.
+.###..##...#
+.###...##.#.
+.###...##..#
+.###....##.#";
+        assert_eq!(525152, process(input))
     }
 }
