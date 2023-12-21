4a1ca360f6773dd929db40db15fb9d78ab9fd7be
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-21 08:09:40 -0300

    Rand Message: /third-wilderness

--- 2023/rust/day21/src/bin/part-1.rs
+++ 2023/rust/day21/src/bin/part-1.rs
@@ -1,26 +1,161 @@
-fn main() {
-    let input = include_str!("../../input.txt");
+use std::collections::VecDeque;
 
-    let starttime = std::time::Instant::now();
+use array2d::{Array2D, Error};
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+use Direction::*;
 
-    let answer = process(input);
-    println!("Part 1 answer: {answer}");
-
-    let elapsed = starttime.elapsed();
-    println!("took {}ms ({}us)", elapsed.as_millis(), elapsed.as_micros());
+#[derive(Debug, PartialEq, EnumIter, Eq, Clone, Copy, Hash, PartialOrd, Ord)]
+enum Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST,
 }
 
-pub fn process(input: &str) -> usize {
-    todo!("comming soon")
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn part1_process() {
-        let input = "";
-        assert_eq!(405, process(input))
+impl Direction {
+    fn delta(&self) -> (i32, i32) {
+        match self {
+            NORTH => (0, -1),
+            EAST => (1, 0),
+            SOUTH => (0, 1),
+            WEST => (-1, 0),
+        }
     }
 }
+
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialOrd, Ord)]
+struct Pos {
+    x: i32,
+    y: i32,
+    distance: u32,
+}
+impl PartialEq for Pos {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+fn is_in_bounds(position: (i32, i32), width: usize, height: usize) -> bool {
+    position.0 >= 0 && position.1 >= 0 && position.0 < width as i32 && position.1 < height as i32
+}
+
+fn get_successors(grid: &Array2D<u8>, pos: Pos) -> Vec<Pos> {
+    Direction::iter()
+        .filter_map(|direction| {
+            let delta = direction.delta();
+            let new_pos = (pos.x + delta.0, pos.y + delta.1);
+            let in_bounds = is_in_bounds(new_pos, grid.num_columns(), grid.num_rows());
+            if !in_bounds {
+                return None;
+            }
+
+            let grid_value = *grid
+                .get(new_pos.1 as usize, new_pos.0 as usize)
+                .expect("found grid tile");
+            if grid_value == b'#' {
+                return None;
+            }
+
+            Some(Pos {
+                x: new_pos.0,
+                y: new_pos.1,
+                distance: pos.distance + 1,
+            })
+        })
+        .collect()
+}
+
+fn part1(input: &str, steps: u32) -> Result<u32, Error> {
+    let lines: Vec<Vec<u8>> = input.lines().map(|line| line.as_bytes().to_vec()).collect();
+
+    let grid = Array2D::from_rows(&lines)?;
+
+    let width = grid.num_columns();
+    let height = grid.num_rows();
+
+    let mut position = Pos {
+        x: 0,
+        y: 0,
+        distance: 0,
+    };
+
+    'outer: for y in 0..height as i32 {
+        for x in 0..width as i32 {
+            let tile = *grid.get(x as usize, y as usize).unwrap();
+            if tile == b'S' {
+                position.x = x;
+                position.y = y;
+                break 'outer;
+            }
+        }
+    }
+
+    let mut queue = VecDeque::new();
+    queue.push_back(position);
+
+    for step in 0..steps {
+        println!("Step {}", step);
+
+        let mut next_steps = VecDeque::new();
+
+        // only handle positions added before this step
+        while !queue.is_empty() {
+            let pos = queue.pop_front().unwrap();
+            for next in get_successors(&grid, pos) {
+                if !next_steps.contains(&next) {
+                    next_steps.push_back(next);
+                }
+            }
+        }
+
+        queue = next_steps;
+    }
+
+    let mut final_steps: Vec<Pos> = Vec::from(queue);
+    println!("Found {} possible paths", final_steps.len());
+    final_steps.sort();
+    final_steps.dedup();
+    println!("Found {} unique final positions", final_steps.len());
+
+    for y in 0..height as i32 {
+        for x in 0..width as i32 {
+            let pos = final_steps.iter().find(|p| p.x == x && p.y == y);
+            if pos.is_some() {
+                print!("O");
+            } else {
+                print!("{}", *grid.get(y as usize, x as usize).unwrap() as char);
+            }
+        }
+        println!();
+    }
+
+    Ok(final_steps.len() as u32)
+}
+
+fn main() {
+    let input = include_str!("../../input.txt");
+    let output = part1(input, 64).expect("found result");
+    dbg!(output);
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn part1_process() {
+//         let input = r#"...........
+// .....###.#.
+// .###.##..#.
+// ..#.#...#..
+// ....#.#....
+// .##..S####.
+// .##..#...#.
+// .......##..
+// .##.#.####.
+// .##..##.##.
+// ..........."#;
+//         assert_eq!(16, process(input))
+//     }
+// }
--- 2023/rust/day21/src/bin/part-2.rs
+++ 2023/rust/day21/src/bin/part-2.rs
@@ -1,30 +1,95 @@
+use polyfit_rs::polyfit_rs::polyfit;
+use std::collections::{BTreeMap, BTreeSet};
+
+fn wrap(x: isize, y: isize, width: isize, height: isize) -> (isize, isize) {
+    let wrap_x = if x < 0 {
+        width - ((x + 1).abs() % width) - 1
+    } else {
+        x % width
+    };
+
+    let wrap_y = if y < 0 {
+        height - ((y + 1).abs() % height) - 1
+    } else {
+        y % height
+    };
+
+    (wrap_x, wrap_y)
+}
+
+fn process(input: &str, steps: isize) -> isize {
+    let mut grid: BTreeMap<(isize, isize), char> = BTreeMap::new();
+    let mut start: (isize, isize) = (0, 0);
+    input.lines().enumerate().for_each(|(y, line)| {
+        line.chars().enumerate().for_each(|(x, c)| {
+            grid.insert((x as isize, y as isize), c);
+            if c == 'S' {
+                start = (x as isize, y as isize);
+                grid.insert((x as isize, y as isize), '.');
+            }
+        });
+    });
+
+    let width = input.lines().next().unwrap().len() as isize;
+    let height = input.lines().count() as isize;
+    assert_eq!(width, height);
+
+    let mut values: Vec<usize> = vec![];
+    let mut plots: BTreeSet<(isize, isize)> = BTreeSet::new();
+    plots.insert(start);
+    for step in 0..steps as isize {
+        let mut next_plots: BTreeSet<(isize, isize)> = BTreeSet::new();
+        for (x, y) in plots.iter() {
+            let neighbors = vec![(*x, *y - 1), (*x, *y + 1), (*x - 1, *y), (*x + 1, *y)];
+            for (nx, ny) in neighbors {
+                let (wrap_x, wrap_y) = wrap(nx, ny, width, height);
+                let neighbor = (wrap_x, wrap_y);
+                if grid.contains_key(&neighbor) {
+                    if grid.get(&neighbor).unwrap() == &'.' {
+                        next_plots.insert((nx, ny));
+                    }
+                }
+            }
+        }
+        plots = next_plots;
+        let offset = start.0 - 1;
+        if step == offset {
+            values.push(plots.len());
+        }
+        if step == offset + width {
+            values.push(plots.len());
+        }
+        if step == offset + 2 * width {
+            values.push(plots.len());
+            break;
+        }
+    }
+
+    let x_values = vec![0.0, 1.0, 2.0];
+    let y_values = values.iter().map(|x| *x as f64).collect::<Vec<_>>();
+    let poly = polyfit(&x_values, &y_values, 2).unwrap();
+    let poly = poly
+        .iter()
+        .map(|x| (x + 0.5).floor() as isize)
+        .collect::<Vec<_>>();
+
+    let n = (steps - start.0) / width;
+
+    poly[2] * n * n + poly[1] * n + poly[0]
+}
+
 fn main() {
     let input = include_str!("../../input.txt");
-
-    let starttime = std::time::Instant::now();
-
-    let answer = process(input);
-    println!("Part 2 answer: {answer}");
-
-    let elapsed = starttime.elapsed();
-    println!(
-        "took {}ms ({}us)  ",
-        elapsed.as_millis(),
-        elapsed.as_micros()
-    );
+    dbg!(process(input, 26501365));
 }
 
-pub fn process(input: &str) -> usize {
-    todo!("comming soon")
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn part1_process() {
-        let input = "";
-        assert_eq!(400, process(input))
-    }
-}
+//     #[test]
+//     fn part1_process() {
+//         let input = "";
+//         assert_eq!(400, process(input))
+//     }
+// }
