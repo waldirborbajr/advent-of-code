4ff8167e5e8bd5e5ec0c7b6cf37286a23393e909
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-12-24 10:31:42 -0300

    Rand Message: /boorish-sail

--- 2023/rust/day24/src/bin/part-1.rs
+++ 2023/rust/day24/src/bin/part-1.rs
@@ -1,26 +1,165 @@
+use itertools::Itertools;
+
 fn main() {
     let input = include_str!("../../input.txt");
 
     let starttime = std::time::Instant::now();
 
-    let answer = process(input);
+    let answer = process(input, 200_000_000_000_000, 400_000_000_000_000);
     println!("Part 1 answer: {answer}");
 
     let elapsed = starttime.elapsed();
     println!("took {}ms ({}us)", elapsed.as_millis(), elapsed.as_micros());
 }
 
-pub fn process(input: &str) -> usize {
-    todo!("comming soon")
+fn process(input: &str, min: i128, max: i128) -> usize {
+    let hailstones = input.lines().map(Hailstone::from).collect::<Vec<_>>();
+
+    let combinations = hailstones
+        .into_iter()
+        .combinations(2)
+        .map(|v| (v[0], v[1]))
+        .collect::<Vec<_>>();
+
+    let intersections = combinations
+        .into_iter()
+        .filter_map(|(a, b)| {
+            if let Some((x, y)) = a.intersection(&b) {
+                // intersection is in past
+                if i128::signum(x - a.x) != i128::signum(a.dx)
+                    || i128::signum(x - b.x) != i128::signum(b.dx)
+                    || i128::signum(y - a.y) != i128::signum(a.dy)
+                    || i128::signum(y - b.y) != i128::signum(b.dy)
+                {
+                    None
+                } else if x >= min && x <= max && y >= min && y <= max {
+                    // intersection is in the specified area
+                    Some((x, y))
+                } else {
+                    // intersection is not in the specified area
+                    None
+                }
+            } else {
+                // no intersection
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    intersections.len()
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+struct Hailstone {
+    x: i128,
+    y: i128,
+    z: i128,
+    dx: i128,
+    dy: i128,
+    dz: i128,
+}
+impl Hailstone {
+    fn new(x: i128, y: i128, z: i128, dx: i128, dy: i128, dz: i128) -> Self {
+        Self {
+            x,
+            y,
+            z,
+            dx,
+            dy,
+            dz,
+        }
+    }
+
+    fn to_line(self) -> (i128, i128, i128) {
+        let a = self.dy;
+        let b = -self.dx;
+        let c = self.dx * self.y - self.dy * self.x;
+        (a, b, c)
+    }
+
+    fn intersection(&self, other: &Self) -> Option<(i128, i128)> {
+        let (a1, b1, c1) = self.to_line();
+        let (a2, b2, c2) = other.to_line();
+
+        if (a1 * b2 - a2 * b1) == 0 {
+            return None;
+        }
+
+        let x = (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
+        let y = (c1 * a2 - c2 * a1) / (a1 * b2 - a2 * b1);
+
+        Some((x, y))
+    }
+}
+impl From<&str> for Hailstone {
+    fn from(value: &str) -> Self {
+        let (pos, vel) = value.split_once(" @ ").expect("no @ symbol");
+        let mut pos = pos.split(", ");
+        let mut vel = vel.split(", ");
+
+        let x = pos
+            .next()
+            .expect("no x")
+            .trim()
+            .parse::<i128>()
+            .expect("x not a number");
+        let y = pos
+            .next()
+            .expect("no y")
+            .trim()
+            .parse::<i128>()
+            .expect("y not a number");
+        let z = pos
+            .next()
+            .expect("no z")
+            .trim()
+            .parse::<i128>()
+            .expect("z not a number");
+        let dx = vel
+            .next()
+            .expect("no dx")
+            .trim()
+            .parse::<i128>()
+            .expect("dx not a number");
+        let dy = vel
+            .next()
+            .expect("no dy")
+            .trim()
+            .parse::<i128>()
+            .expect("dy not a number");
+        let dz = vel
+            .next()
+            .expect("no dz")
+            .trim()
+            .parse::<i128>()
+            .expect("dz not a number");
+
+        Hailstone::new(x, y, z, dx, dy, dz)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
 
     #[test]
-    fn part1_process() {
-        let input = "";
-        assert_eq!(405, process(input))
+    fn test_part1() {
+        assert_eq!(
+            process(
+                indoc!(
+                    "
+                    19, 13, 30 @ -2,  1, -2
+                    18, 19, 22 @ -1, -1, -2
+                    20, 25, 34 @ -2, -2, -4
+                    12, 31, 28 @ -1, -2, -1
+                    20, 19, 15 @  1, -5, -3
+                    "
+                ),
+                7,
+                27
+            ),
+            2
+        );
     }
 }
--- 2023/rust/day24/src/bin/part-2.rs
+++ 2023/rust/day24/src/bin/part-2.rs
@@ -1,30 +1,91 @@
+use z3::ast::Ast;
+
 fn main() {
     let input = include_str!("../../input.txt");
 
     let starttime = std::time::Instant::now();
 
     let answer = process(input);
-    println!("Part 2 answer: {answer}");
+    println!("Part 1 answer: {answer}");
 
     let elapsed = starttime.elapsed();
-    println!(
-        "took {}ms ({}us)  ",
-        elapsed.as_millis(),
-        elapsed.as_micros()
-    );
+    println!("took {}ms ({}us)", elapsed.as_millis(), elapsed.as_micros());
 }
 
-pub fn process(input: &str) -> usize {
-    todo!("comming soon")
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Hailstone {
+    position: (i64, i64, i64),
+    velocity: (i64, i64, i64),
+}
+
+fn parse(input: &str) -> Vec<Hailstone> {
+    input.lines().map(|line| parse_line(line)).collect()
+}
+
+fn parse_line(line: &str) -> Hailstone {
+    let numbers: Vec<i64> = line
+        .split(|c: char| !c.is_digit(10) && c != '-')
+        .filter(|&s| !s.is_empty())
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+    Hailstone {
+        position: (numbers[0], numbers[1], numbers[2]),
+        velocity: (numbers[3], numbers[4], numbers[5]),
+    }
+}
+
+fn process(input: &str) -> i64 {
+    let hailstones = parse(input);
+    let cfg = z3::Config::new();
+    let context = z3::Context::new(&cfg);
+    let solver = z3::Solver::new(&context);
+
+    let x = z3::ast::Int::new_const(&context, "x");
+    let y = z3::ast::Int::new_const(&context, "y");
+    let z = z3::ast::Int::new_const(&context, "z");
+    let vx = z3::ast::Int::new_const(&context, "vx");
+    let vy = z3::ast::Int::new_const(&context, "vy");
+    let vz = z3::ast::Int::new_const(&context, "vz");
+
+    for (i, hs) in hailstones.iter().take(3).enumerate() {
+        let a = z3::ast::Int::from_i64(&context, hs.position.0);
+        let va = z3::ast::Int::from_i64(&context, hs.velocity.0);
+        let b = z3::ast::Int::from_i64(&context, hs.position.1);
+        let vb = z3::ast::Int::from_i64(&context, hs.velocity.1);
+        let c = z3::ast::Int::from_i64(&context, hs.position.2);
+        let vc = z3::ast::Int::from_i64(&context, hs.velocity.2);
+
+        let t = z3::ast::Int::new_const(&context, format!("t{i}"));
+        solver.assert(&t.gt(&z3::ast::Int::from_i64(&context, 0)));
+        solver.assert(&(x.clone() + vx.clone() * t.clone())._eq(&(a + va * t.clone())));
+        solver.assert(&(y.clone() + vy.clone() * t.clone())._eq(&(b + vb * t.clone())));
+        solver.assert(&(z.clone() + vz.clone() * t.clone())._eq(&(c + vc * t.clone())));
+    }
+    if solver.check() == z3::SatResult::Sat {
+        let m = solver.get_model().unwrap();
+        return m.eval(&(x + y + z), true).unwrap().as_i64().unwrap();
+    }
+    panic!("Failed to solve!")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
 
     #[test]
-    fn part1_process() {
-        let input = "";
-        assert_eq!(400, process(input))
+    fn test_part2() {
+        assert_eq!(
+            process(indoc!(
+                "
+                    19, 13, 30 @ -2,  1, -2
+                    18, 19, 22 @ -1, -1, -2
+                    20, 25, 34 @ -2, -2, -4
+                    12, 31, 28 @ -1, -2, -1
+                    20, 19, 15 @  1, -5, -3
+                    "
+            )),
+            47
+        );
     }
 }
